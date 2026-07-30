19e8cd0ddf967e560696d8c7d2f5801b760ea4a6
rabiyakhalid13 <rabiyaakhalid13@gmail.com>
2026-07-30 22:00:04 +0500

    Commit All

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -153,10 +153,4 @@ def update_table(selected_coin):
 server = app.server
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(
-        host="0.0.0.0",
-        port=port,
-        debug=False
-    )
-    
+    app.run(debug=True)
